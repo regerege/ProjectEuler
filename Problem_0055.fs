03ddc1e3943cd18397dabd249452a4772f5478f7
regerege <regerege6@gmail.com>
2012-07-28 17:37:07 +0900

    Add Problem 55

---
+++ Problem_0055.fs
@@ -0,0 +1,23 @@
+﻿module Problem_0055
+
+let rev (x:bigint) = bigint.Parse(System.String((string x).ToCharArray() |> Array.rev))
+let take x =
+    Seq.init 49 id
+    |> Seq.scan (fun acm _ -> acm + rev acm) x
+    |> Seq.skip 1
+let calc() =
+    Seq.init 9999 ((+)1)
+    |> Seq.map (fun x -> bigint(x))
+    |> Seq.filter (fun x ->
+        take x
+        |> Seq.tryFind (fun x -> x = rev x)
+        |> Option.isNone
+    ) |> Seq.length
+
+let run() = calc()
+
+(*
+> run();;
+リアル: 00:00:00.332、CPU: 00:00:00.249、GC gen0: 22, gen1: 0, gen2: 0
+val it : int = 249
+*)
