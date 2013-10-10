7d54549988b2ed3f57dd27367ae5551be9823096
regerege <regerege6@gmail.com>
2013-10-10 22:12:53 +0900

    Add Problem 7 for PostgreSQL

--- PostgreSQL/Problem0003.sql
+++ PostgreSQL/Problem0003.sql
@@ -1,4 +1,4 @@
-/*
+﻿/*
 13195 の素因数は 5, 7, 13, 29 である.
 600851475143 の素因数のうち最大のものを求めよ.
 http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%203
---
+++ PostgreSQL/Problem0007.sql
@@ -0,0 +1,27 @@
+﻿/*
+generate_series
+素数を小さい方から6つ並べると 2, 3, 5, 7, 11, 13 であり,
+6番目の素数は 13 である.
+10001 番目の素数を求めよ.
+http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%207
+*/
+WITH RECURSIVE temp(n,ps) AS (
+  SELECT 2,ARRAY[2]::int[]
+UNION ALL
+  SELECT n+1
+    , (CASE
+         WHEN
+           NOT EXISTS(
+             SELECT *
+             FROM (SELECT unnest(ps)) a
+             WHERE (n+1) % a.unnest = 0
+           )
+         THEN ps || (n+1)
+         ELSE ps
+       END)
+  FROM temp
+  WHERE array_length(ps,1) < 10001
+)
+SELECT ps[array_length(ps,1)]
+FROM temp a
+WHERE a.n = (SELECT MAX(b.n) FROM temp b)
