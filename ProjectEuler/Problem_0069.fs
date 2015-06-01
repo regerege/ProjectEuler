5e360f792658f2b979cc6279284e5f81404a6c50
regerege <regerege6@gmail.com>
2015-06-01 20:58:11 +0900

    Add Problem 69 and 70

---
+++ ProjectEuler/Problem_0069.fs
@@ -0,0 +1,71 @@
+﻿module Problem_0069
+(*
+http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2069
+
+オイラーのトーティエント関数, φ(n) [時々ファイ関数とも呼ばれる]は, n と互いに素な n 未満の数の数を定める. たとえば, 1, 2, 4, 5, 7, そして8はみな9未満で9と互いに素であり, φ(9)=6.
+n	互いに素な数	φ(n)	n/φ(n)
+2	1	1	2
+3	1,2	2	1.5
+4	1,3	2	2
+5	1,2,3,4	4	1.25
+6	1,5	2	3
+7	1,2,3,4,5,6	6	1.1666...
+8	1,3,5,7	4	2
+9	1,2,4,5,7,8	6	1.5
+10	1,3,7,9	4	2.5
+n ≤ 10 では n/φ(n) の最大値は n=6 であることがわかる.
+n ≤ 1,000,000で n/φ(n) が最大となる値を見つけよ.
+
+> part1();;
+Real: 00:15:15.157, CPU: 00:15:15.437, GC gen0: 119694, gen1: 66000, gen2: 1101
+val it : int * int = (30030, 5760)
+> part2();;
+Real: 00:00:00.009, CPU: 00:00:00.015, GC gen0: 0, gen1: 0, gen2: 0
+val it : int * int = (510510, 92160)
+*)
+
+#nowarn "40"
+///素数シーケンス
+let rec primes = 
+    Seq.cache <| seq { yield 2; yield! Seq.unfold nextPrime 3 }
+and nextPrime n =
+    if isPrime n then Some(n, n + 2) else nextPrime(n + 2)
+and isPrime n =
+    if n >= 2 then
+        primes 
+        |> Seq.tryFind (fun x -> n % x = 0 || x * x > n)
+        |> fun x -> x.Value * x.Value > n
+    else false
+let part2() =
+    primes
+    |> Seq.scan(fun (c,(a,b)) n ->
+        let a = a*n
+        let b = b*(n-1)
+        (if b = 0 then 0M else (decimal a / decimal b)),(a,b)
+        ) (0M,(1,1))
+    |> Seq.takeWhile (snd >> fst >> ((>=)1000000))
+    |> Seq.maxBy fst
+    |> snd
+
+let pf ((n,pfl),pl) =
+    let rec pf2 p n pfl pl =
+        match n,pl with
+        | _,[] | 1,_ -> pfl
+        | _,x::xs ->
+            match (n%x,n/x) with
+            | 0,x2 -> pf2 x x2 (if x <> p then pfl@[x] else pfl) pl
+            | _,_ -> pf2 p n pfl xs
+    let l = pf2 1 n [] (pl@[n])
+    let l2 = if List.length l = 1 then pl@[n] else pl
+    let a = (n,l),l2
+    let b = (n+1,l),l2
+    Some (a,b)
+let totient ((n:int),l) = n,List.fold (fun n x -> (n-1)*(n/x)) n l
+let part1() =
+    Seq.unfold pf ((1,[1]),[])
+    |> Seq.take 1000000
+    |> Seq.map (fst >> totient)
+    |> Seq.maxBy (fun (n,x) -> bigint n / bigint x)
+
+let run() =
+    part1()
---
+++ ProjectEuler/Problem_0070.fs
@@ -0,0 +1,98 @@
+﻿module Problem_0070
+(*
+http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2070
+
+オイラーのトーティエント関数 φ(n) (ファイ関数とも呼ばれる) とは,
+n 未満の正の整数で n と互いに素なものの個数を表す.
+例えば, 1, 2, 4, 5, 7, 8 は9未満で9と互いに素であるので, φ(9) = 6 となる. 
+1 は全ての正の整数と互いに素であるとみなされる. よって φ(1) = 1 である.
+
+面白いことに, φ(87109)=79180 であり, 87109は79180を置換したものとなっている.
+
+1 < n < 10^7 で φ(n) が n を置換したものになっているもののうち, n/φ(n) が最小となる n を求めよ.
+
+> part2();;
+Real: 00:00:00.832, CPU: 00:00:00.843, GC gen0: 230, gen1: 1, gen2: 0
+val it : int * int = (8319823, 8313928)
+*)
+
+///////////////////////////////////////////////////////////////////////////////////
+
+#nowarn "40"
+let rec primes = 
+    Seq.cache <| seq { yield 2; yield! Seq.unfold nextPrime 3 }
+and nextPrime n =
+    if isPrime n then Some(n, n + 2) else nextPrime(n + 2)
+and isPrime n =
+    if n >= 2 then
+        primes 
+        |> Seq.tryFind (fun x -> n % x = 0 || x * x > n)
+        |> fun x -> x.Value * x.Value > n
+    else false
+// source code
+// http://stackoverflow.com/questions/4495597/combinations-and-permutations-in-f
+let rec combinations acc size set = seq {
+  match size, set with 
+  | n, x::xs -> 
+      if n > 0 then yield! combinations (x::acc) (n - 1) xs
+      if n >= 0 then yield! combinations acc n xs 
+  | 0, [] -> yield acc 
+  | _, [] -> () }
+(*
+素数の集合 S
+10,000,000 以下の素数を2つ用意する。
+2つの素因数がでかければでかいほど結果は小さくなるので範囲を 2,000 ～ 5,000 とする。
+（1,000から10,000までの範囲を扱っても、10^7以上の数が多く無駄な気がするのも理由の一つ）
+366_C_2 = 66,795 の計算処理で結果が求まる。
+*)
+let part2() =
+    let list =
+        primes
+        |> Seq.takeWhile ((>)5000)
+        |> Seq.filter ((<)2000)
+        |> Seq.toList
+        |> List.rev
+    combinations [] 2 list
+    |> Seq.map (fun pl -> pl.[0]*pl.[1], (pl.[0]-1)*(pl.[1]-1))
+    |> Seq.filter (fst >> ((>)10000000))
+    |> Seq.filter (fun (a,b) ->
+        let a = string a
+        let b = string b
+        if a.Length <> b.Length then false
+        else
+            let a = Seq.map string a |> Seq.sort |> Seq.reduce(+)
+            let b = Seq.map string b |> Seq.sort |> Seq.reduce(+)
+            a = b)
+    |> Seq.minBy(fun (a,b) -> float a / float b)
+
+///////////////////////////////////////////////////////////////////////////////////
+
+let totient ((n:int),(l:int list)) = List.fold (fun n x -> (x-1)*(n/x)) n l
+let fac n =
+    let rec f n a l =
+        if n = 1 then l
+        else
+            match (n%a),(n/a) with
+            | 0,x -> f x a (l@[a])
+            | _,_ -> f n (if a = 2 then a+1 else a+2) l
+    f n 2 []
+let part1() =
+    Seq.initInfinite ((+)1)
+    |> Seq.takeWhile((>=)10000000)
+    |> Seq.map (fun n -> n,fac n)
+    |> Seq.map (fun (n,f) -> n,(f,totient (n,f |> Set.ofList |> Seq.toList)))
+    |> Seq.filter (fun (a,(_,b)) ->
+        let a = string a
+        let b = string b
+        if a.Length <> b.Length then false
+        else
+            let a = Seq.map string a |> Seq.sort |> Seq.reduce(+)
+            let b = Seq.map string b |> Seq.sort |> Seq.reduce(+)
+            a = b)
+    |> Seq.map (fun (a,(b,c)) -> a,c,(float a / float c),b.Length,b)
+    |> Seq.minBy (fun (_,_,a,_,_) -> a)
+    
+///////////////////////////////////////////////////////////////////////////////////
+
+let run() =
+    part2()
